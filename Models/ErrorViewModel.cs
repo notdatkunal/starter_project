9b85cfb956889e933ed02e04555cc09a75a312cb
Kunal Dabhane <kunaldabhane20012@gmail.com>
2024-10-20 17:56:11 +0530

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace starter_project.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
